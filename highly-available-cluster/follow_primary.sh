bf7ff9d567a2e4e67322031eec5a2edb7b22eacf
root <salman.ahmed@stormatics.tech>
2023-04-26 01:06:23 +0500

    Folder name updated to highly-available-cluster

---
+++ highly-available-cluster/follow_primary.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# follow_primary.sh
+# This script is executed by follow_primary_command parameter used by pgpool.conf file. This fil will be executed on all standby nodes including new primary node as well.
+# We are updating replication properties from all standby nodes to the new primary node
+
+
+set -o xtrace
+
+# Special values:
+# 1)  %d = failed node id
+# 2)  %h = failed node hostname
+# 3)  %p = failed node port number
+# 4)  %D = failed node database cluster path
+# 5)  %m = new main node id
+# 6)  %H = new main node hostname
+# 7)  %M = old main node id
+# 8)  %P = old primary node id
+# 9)  %r = new main port number
+# 10) %R = new main database cluster path
+# 11) %N = old primary node hostname
+# 12) %S = old primary node port number
+# 13) %% = '%' character
+
+NODE_ID="$1"
+NODE_HOST="$2"
+NODE_PORT="$3"
+NODE_PGDATA="$4"
+NEW_MAIN_NODE_ID="$5"
+NEW_MAIN_NODE_HOST="$6"
+OLD_MAIN_NODE_ID="$7"
+OLD_PRIMARY_NODE_ID="$8"
+NEW_MAIN_NODE_PORT="$9"
+NEW_MAIN_NODE_PGDATA="${10}"
+OLD_PRIMARY_NODE_HOST="${11}"
+OLD_PRIMARY_NODE_PORT="${12}"
+ 
+if [[ "$NODE_HOST" == "$OLD_PRIMARY_NODE_HOST" ]]; then
+	echo nothing to do
+	exit 0
+fi
+
+# Creates a new replication slot on new primary
+ssh postgres@$NEW_MAIN_NODE_HOST "echo 123 | psql -d postgres -W -c \"SELECT pg_create_physical_replication_slot('sb2');\""
+
+# Updates connection string on all standbys to point to new primary
+ssh postgres@$NODE_HOST "echo 123 |  psql -d postgres -W -c \"ALTER SYSTEM SET primary_conninfo = 'user=repuser password=repuserpassword channel_binding=prefer host=$NEW_MAIN_NODE_HOST port=5432 sslmode=prefer sslcompression=0 sslsni=1 ssl_min_protocol_version=TLSv1.2 gssencmode=prefer krbsrvname=postgres target_session_attrs=any';\""
+
+# Reload connection properties
+ssh postgres@$NODE_HOST "echo 123 | psql -d postgres -W -c \"SELECT pg_reload_conf();\""
