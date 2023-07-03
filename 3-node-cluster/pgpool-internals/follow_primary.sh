7dea2d8497604b50a133a70f976658c4d698070e
root <salman.ahmed@stormatics.tech>
2023-07-03 15:02:59 +0500

    Hardcoded port 5432 replaced with node port inside follow_primary.sh file

--- 3-node-cluster/pgpool-internals/follow_primary.sh
+++ 3-node-cluster/pgpool-internals/follow_primary.sh
@@ -41,12 +41,14 @@ if [[ "$NODE_HOST" == "$OLD_PRIMARY_NODE_HOST" ]]; then
 	exit 0
 fi
 
+echo NODE_PORT
+
 # Create a new replication slot on new primary
 echo creatig rep slot on $NEW_MAIN_NODE_HOST with the name of $SLOT_NAME
 ssh postgres@$NEW_MAIN_NODE_HOST "psql -d postgres -w -c \"SELECT pg_create_physical_replication_slot('slot_$SLOT_NAME');\""
 
 # Update connection string on all standby nodes to point to new primary
-ssh postgres@$NODE_HOST "psql -d postgres -w -c \"ALTER SYSTEM SET primary_conninfo = 'user=repuser host=$NEW_MAIN_NODE_HOST port=5432';\""
+ssh postgres@$NODE_HOST "psql -d postgres -w -c \"ALTER SYSTEM SET primary_conninfo = 'user=repuser host=$NEW_MAIN_NODE_HOST port=$NODE_PORT';\""
 
 # Reload connection properties
 ssh postgres@$NODE_HOST "psql -d postgres -w -c \"SELECT pg_reload_conf();\""
